0591f403766930a56e1223069210aa9ec42a389d
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2020-12-05 15:00:27 +0700

    add more info in setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="Shared_Function",
     version="0.0.9",
+    license = "MIT",	
     author="Chanon Krittapholchai",
     author_email="chanon.krittapholchai@gmail.com",
     description="A shared function that use within my team",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chanon-kr/Shared_Function",
+    keywords = ["utility"],
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 1 - Planning",
