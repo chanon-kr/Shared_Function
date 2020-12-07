3b27e941a314ed93b0dc899c72e4a693b3d87b13
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2020-12-07 16:46:40 +0700

    Update Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_topping",
-    version="0.1.0",
+    version="0.1.01",
     license = "MIT",	
     author="Chanon Krittapholchai",
     author_email="chanon.krittapholchai@gmail.com",
