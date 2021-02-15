2f49da7fd275e5b7cad80df120d14bef2b234884
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2021-02-15 09:16:04 +0700

    Reverse Version back to 0.2.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="py_topping",
-    version="0.2.3",
+    version="0.2.2",
     license = "MIT",	
     author="Chanon Krittapholchai",
     author_email="chanon.krittapholchai@gmail.com",
