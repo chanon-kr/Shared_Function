4d3310817a0c1c12e7b7b32ca903119a1f4f3a60
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2021-02-15 09:17:55 +0700

    Update Version to 0.2.3 and Update URL

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="py_topping",
-    version="0.2.2",
+    version="0.2.3",
     license = "MIT",	
     author="Chanon Krittapholchai",
     author_email="chanon.krittapholchai@gmail.com",
@@ -20,7 +20,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chanon-kr/Shared_Function",
-    download_url = "https://github.com/chanon-kr/Shared_Function/archive/0.2.2.tar.gz",
+    download_url = "https://github.com/chanon-kr/Shared_Function/archive/0.2.3.tar.gz",
     keywords = ["utility"],
     packages=setuptools.find_packages(),
     classifiers=[
