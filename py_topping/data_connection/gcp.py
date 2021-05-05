60dbd0869049c697a408ecab10afbc5cc1abcf92
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2021-05-05 09:27:11 +0700

    Add class da_tran_bucket to download/upload file from gcp bucket

---
+++ py_topping/data_connection/gcp.py
@@ -0,0 +1,35 @@
+import os
+from google.cloud import storage
+
+
+class da_tran_gcp:
+    def __init__(self, project_id, bucket_name,credentials = '') :
+        self.project_id = project_id
+        self.bucket_name = bucket_name
+        self.credentials = credentials
+
+    def download(self, bucket_file, local_file):
+        if self.credentials == '' : pass
+        else : os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= self.credentials
+
+        # Initialise a client
+        client = storage.Client(self.project_id)
+        # Create a bucket object for our bucket
+        bucket = client.get_bucket(self.bucket_name)
+        # Create a blob object from the filepath
+        blob = bucket.blob(bucket_file)
+        # Download the file to a destination
+        blob.download_to_filename(local_file)
+    
+    def upload(self, bucket_file, local_file):
+        if self.credentials == '' : pass
+        else : os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= self.credentials
+
+        # Initialise a client
+        client = storage.Client(self.project_id)
+        # Create a bucket object for our bucket
+        bucket = client.get_bucket(self.bucket_name)
+        # Create a blob object from the filepath
+        blob = bucket.blob(bucket_file)
+        # Download the file to a destination
+        blob.upload_from_filename(local_file)
