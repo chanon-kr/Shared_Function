e6694871a8f170b107987798248109cda7bf7bc7
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2021-05-09 16:48:43 +0700

    Update Donwload Link

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/chanon-kr/Shared_Function",
-    download_url = "https://github.com/chanon-kr/Shared_Function/archive/0.3.1.tar.gz",
+    download_url = "https://github.com/chanon-kr/Shared_Function/archive/0.3.2.tar.gz",
     keywords = ["utility","ETL"],
     packages=setuptools.find_packages(),
     classifiers=[
