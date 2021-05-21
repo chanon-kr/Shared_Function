621ecb2d0a1d2fb703eda0b4d61c40343e4e16bc
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2021-05-21 14:41:15 +0700

    comment non-test function

---
+++ py_topping/__init__.py
@@ -0,0 +1,5 @@
+### Not Test
+# from .data_connection.database import da_tran_SQL as lazy_SQL
+# from .data_connection.sharepoint import da_tran_SP365 as lazy_SP365
+# from .data_connection.sharepoint import da_tran_SP_PRIM as lazy_SP_PRIM
+# from .run_pipeline import run_pipeline
