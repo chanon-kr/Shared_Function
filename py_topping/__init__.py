0ef0107fce77ba17422dba0191bff8f3ce510427
chanon-kr <64777509+chanon-kr@users.noreply.github.com>
2021-06-02 15:23:12 +0700

    __init__ > add commented code (not use now)

--- py_topping/__init__.py
+++ py_topping/__init__.py
@@ -1,5 +1,9 @@
 ### Not Test
-# from .data_connection.database import da_tran_SQL as lazy_SQL
+# def lazy_SQL(sql_type, host_name, database_name, user = '', password = '' , credentials_path = ''
+#                , chunksize = 150, partition_size = 5000, parallel_dump = False, max_parallel = 2) :
+#     from .data_connection.database import da_tran_SQL
+#     return da_tran_SQL(sql_type, host_name, database_name, user , password , credentials_path 
+#                 , chunksize , partition_size , parallel_dump , max_parallel )
 # from .data_connection.sharepoint import da_tran_SP365 as lazy_SP365
 # from .data_connection.sharepoint import da_tran_SP_PRIM as lazy_SP_PRIM
 # from .run_pipeline import run_pipeline
