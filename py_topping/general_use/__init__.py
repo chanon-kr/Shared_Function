2b0bf704b97e4dbf47573ccfde067066e2dc89ec
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-01-12 11:35:52 +0700

    Add timeout function in general_use

--- py_topping/general_use/__init__.py
+++ py_topping/general_use/__init__.py
@@ -150,3 +150,47 @@ def healthcheck(cpu_interval = 1):
              'cpu_use': [cpu_use], 
              'ram_use': [ram_use],
              'rom_use': [rom_use]}
+
+
+
+def timeout(timeout):
+    """
+    Refer to https://stackoverflow.com/questions/21827874/timeout-a-function-windows
+    
+    Usage
+    
+    func = timeout(timeout=XXX)(My_Function)
+    try: func()
+    except: pass
+
+    or
+
+    @timeout(XX)
+    def My_Function :
+        ....
+    """
+    from threading import Thread
+    import functools
+    def deco(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            res = [Exception('function [%s] timeout [%s seconds] exceeded!' % (func.__name__, timeout))]
+            def newFunc():
+                try:
+                    res[0] = func(*args, **kwargs)
+                except Exception as e:
+                    res[0] = e
+            t = Thread(target=newFunc)
+            t.daemon = True
+            try:
+                t.start()
+                t.join(timeout)
+            except Exception as je:
+                print ('error starting thread')
+                raise je
+            ret = res[0]
+            if isinstance(ret, BaseException):
+                raise ret
+            return ret
+        return wrapper
+    return deco
