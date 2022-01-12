9725339967a5382bf447f6b3f376a1c85478de24
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-01-12 12:58:25 +0700

    change version to 0.3.15

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="py_topping",
-    version="0.3.14",
+    version="0.3.15",
     license = "MIT",	
     author="Chanon Krittapholchai",
     author_email="chanon.krittapholchai@gmail.com",
