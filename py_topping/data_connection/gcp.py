dfe654d3986b69465593944266964563134418ff
Chanon Krittapholchai <64777509+chanon-kr@users.noreply.github.com>
2022-04-18 15:54:36 +0700

    Add signed_urls function in lazy_GCS

--- py_topping/data_connection/gcp.py
+++ py_topping/data_connection/gcp.py
@@ -1,12 +1,101 @@
 import os
 from google.cloud import storage
 from glob import glob
+import binascii
+import collections
+import datetime
+import hashlib
+import sys
+
+# pip install google-auth
+from google.oauth2 import service_account
+# pip install six
+import six
+from six.moves.urllib.parse import quote
 
 class lazy_GCS :
     def __init__(self, project_id, bucket_name,credential = '') :
         self.project_id = project_id
         self.bucket_name = bucket_name
         self.credentials = credential
+        
+    def generate_signed_url(self, service_account_file, bucket_name, object_name,
+                        subresource=None, expiration=604800, http_method='GET',
+                        query_parameters=None, headers=None):
+        """Reference from https://github.com/GoogleCloudPlatform/python-docs-samples/tree/e7379a8b33ab29b354292d370c3655f481bf3be5/storage/signed_urls"""
+        if expiration > 604800:
+            print('Expiration Time can\'t be longer than 604800 seconds (7 days).')
+            sys.exit(1)
+        escaped_object_name = quote(six.ensure_binary(object_name), safe=b'/~')
+        canonical_uri = f'/{escaped_object_name}'
+
+        datetime_now = datetime.datetime.now(tz=datetime.timezone.utc)
+        request_timestamp = datetime_now.strftime('%Y%m%dT%H%M%SZ')
+        datestamp = datetime_now.strftime('%Y%m%d')
+
+        google_credentials = service_account.Credentials.from_service_account_file(service_account_file)
+        client_email = google_credentials.service_account_email
+        credential_scope = f'{datestamp}/auto/storage/goog4_request'
+        credential = f'{client_email}/{credential_scope}'
+
+        if headers is None: headers = dict()
+        host = f'{bucket_name}.storage.googleapis.com'
+        headers['host'] = host
+
+        canonical_headers = ''
+        ordered_headers = collections.OrderedDict(sorted(headers.items()))
+        for k, v in ordered_headers.items():
+            lower_k = str(k).lower()
+            strip_v = str(v).lower()
+            canonical_headers += f'{lower_k}:{strip_v}\n'
+
+        signed_headers = ''
+        for k, _ in ordered_headers.items():
+            lower_k = str(k).lower()
+            signed_headers += f'{lower_k};'
+        signed_headers = signed_headers[:-1]  # remove trailing ';'
+
+        if query_parameters is None: query_parameters = dict()
+        query_parameters['X-Goog-Algorithm'] = 'GOOG4-RSA-SHA256'
+        query_parameters['X-Goog-Credential'] = credential
+        query_parameters['X-Goog-Date'] = request_timestamp
+        query_parameters['X-Goog-Expires'] = expiration
+        query_parameters['X-Goog-SignedHeaders'] = signed_headers
+        if subresource:
+            query_parameters[subresource] = ''
+
+        canonical_query_string = ''
+        ordered_query_parameters = collections.OrderedDict(
+            sorted(query_parameters.items()))
+        for k, v in ordered_query_parameters.items():
+            encoded_k , encoded_v = quote(str(k), safe='') , quote(str(v), safe='') # C Modify
+            canonical_query_string += f'{encoded_k}={encoded_v}&'
+        canonical_query_string = canonical_query_string[:-1]  # remove trailing '&'
+
+        canonical_request = '\n'.join([http_method,
+                                       canonical_uri,
+                                       canonical_query_string,
+                                       canonical_headers,
+                                       signed_headers,
+                                       'UNSIGNED-PAYLOAD'])
+
+        canonical_request_hash = hashlib.sha256(
+            canonical_request.encode()).hexdigest()
+
+        string_to_sign = '\n'.join(['GOOG4-RSA-SHA256',
+                                    request_timestamp,
+                                    credential_scope,
+                                    canonical_request_hash])
+
+        # signer.sign() signs using RSA-SHA256 with PKCS1v15 padding
+        signature = binascii.hexlify(
+            google_credentials.signer.sign(string_to_sign)
+        ).decode()
+
+        scheme_and_host = f'https://{host}'
+        signed_url = f'{scheme_and_host}{canonical_uri}?{canonical_query_string}&x-goog-signature={signature}'
+
+        return signed_url
 
     def download(self, bucket_file, local_file):
         if self.credentials == '' : pass
