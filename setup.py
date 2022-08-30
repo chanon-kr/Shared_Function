1b406da7d0d278f5b44c97d8818e1969b8bbf60f
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2022-08-30 15:04:22 +0700

    Update version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
 
 setuptools.setup(
     name="py_topping",
-    version="0.4.3",
+    version="0.4.4",
     license = "MIT",	
     author="Chanon Krittapholchai",
     author_email="chanon.krittapholchai@gmail.com",
